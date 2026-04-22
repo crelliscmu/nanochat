26cf92da8bf6c189e62a90a9b1d139a87909e88e
crelliscmu <crellis@andrew.cmu.edu>
2026-04-22 17:03:33 -0400

    colab compatibility

--- install.sh
+++ install.sh
@@ -1,12 +1,21 @@
 #!/bin/bash
 set -euo pipefail
 
+if [ -n "$COLAB_RELEASE_TAG" ]; then
+    export UV_SYSTEM_PYTHON='false'
+    echo "export HOME=/content" >> /content/nanochat/.venv/bin/activate
+    export ARTIFACTS_DIR=/content/nanochat/nanochat_artifacts
+    mkdir -p $ARTIFACTS_DIR
+else
+    echo "Not a Colab release tag"
+fi
+
 REPO_ROOT="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 cd "$REPO_ROOT"
 
-command -v uv &> /dev/null || curl -LsSf https://astral.sh/uv/install.sh | sh
-[ -d ".venv" ] || uv venv
 uv sync --extra gpu --extra longcontext
+
+echo "export HOME=/content" >> /content/nanochat/.venv/bin/activate
 source .venv/bin/activate
 
 echo "Patching installed transformers with NanoChat model"
